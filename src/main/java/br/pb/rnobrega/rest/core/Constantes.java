e26c2831c8f1a2d023110c5c789319f4c92b6afb
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-25 11:17:48 -0300

    Criando a estrutura inicial do projeto

---
+++ src/main/java/br/pb/rnobrega/rest/core/Constantes.java
@@ -0,0 +1,14 @@
+package br.pb.rnobrega.rest.core;
+
+import io.restassured.http.ContentType;
+
+public interface Constantes {
+
+    String APP_BASE_URL = "https://barrigarest.wcaquino.me/";
+    Integer APP_PORT = 443; //para HTTP seria 80
+    String APP_BASE_PATH = "";
+
+    ContentType APP_CONTENT_TYPE = ContentType.JSON;
+
+    Long MAX_TIMEOUT = 1000L;
+}
