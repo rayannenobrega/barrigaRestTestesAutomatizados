c120f17c22997d4d85e63554e33439704c449162
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-25 13:59:55 -0300

    Deve alterar conta com sucesso

--- src/main/java/br/pb/rnobrega/rest/tests/BarrigaTest.java
+++ src/main/java/br/pb/rnobrega/rest/tests/BarrigaTest.java
@@ -1,6 +1,7 @@
 package br.pb.rnobrega.rest.tests;
 
 import br.pb.rnobrega.rest.core.BaseTest;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -12,6 +13,27 @@ import static org.hamcrest.Matchers.*;
 
 //É necessário extender o BaseTest que criamos.
 public class BarrigaTest extends BaseTest {
+
+    private String TOKEN;
+
+    @Before
+    public void loginToken(){
+        //Mapeando o Login
+        Map<String, String> login = new HashMap<String, String >();
+        login.put("email", "rayanne@email");
+        login.put("senha", "casa123");
+
+        //Login na API e recebimento de TOKEN
+        TOKEN = given()
+                .body(login) //Passando o login em formato de MAP para melhor organização
+                .when()
+                .post("/signin")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().path("token")
+        ;
+    }
 
     @Test
     public void naoDeveAcessarAPISemToken(){
@@ -26,26 +48,8 @@ public class BarrigaTest extends BaseTest {
     @Test
     public void deveIncluirContaComSucesso(){
 
-        //Mapeando o Login
-        Map<String, String> login = new HashMap<String, String >();
-        login.put("email", "rayanne@email");
-        login.put("senha", "casa123");
-
-        //Login na API e recebimento de TOKEN
-        String token = given()
-                .body(login) //Passando o login em formato de MAP para melhor organização
-        .when()
-                .post("/signin")
-        .then()
-                .statusCode(200)
-                .extract().path("token")
-        ;
-        //System.out.println(token);
-
-        //Incluir conta
-
         given()
-                .header("Authorization", "JWT " + token) //API's mais recentes ao inves de 'JWT' se usa 'bearer'.
+                .header("Authorization", "JWT " + TOKEN) //API's mais recentes ao inves de 'JWT' se usa 'bearer'.
                 .body("{\"nome\": \"conta qualquer\"}") //Passa o JSON com as informações que se quer
         .when()
                 .post("/contas")//Método post para envio
@@ -54,6 +58,19 @@ public class BarrigaTest extends BaseTest {
         ;
 
     }
-}
 
+    @Test
+    public void deveAlterarContaComSucesso(){
+
+        given()
+                .header("Authorization", "JWT " + TOKEN) //API's mais recentes ao inves de 'JWT' se usa 'bearer'.
+                .body("{\"nome\": \"conta alterada\"}")
+        .when()
+                .put("/contas/658079")//comando do tipo PUT para alteração para /contas/:id
+        .then()
+                .statusCode(200)
+                .body("nome", is("conta alterada"))
+        ;
+    }
+}
 
