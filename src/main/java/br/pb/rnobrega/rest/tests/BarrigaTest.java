165b0f725dac9170d23e938f7e93d635e3188428
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-25 12:10:56 -0300

    deve incluir conta com sucesso

--- src/main/java/br/pb/rnobrega/rest/tests/BarrigaTest.java
+++ src/main/java/br/pb/rnobrega/rest/tests/BarrigaTest.java
@@ -3,6 +3,9 @@ package br.pb.rnobrega.rest.tests;
 import br.pb.rnobrega.rest.core.BaseTest;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -19,4 +22,38 @@ public class BarrigaTest extends BaseTest {
                 .statusCode(401)
         ;
     }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+
+        //Mapeando o Login
+        Map<String, String> login = new HashMap<String, String >();
+        login.put("email", "rayanne@email");
+        login.put("senha", "casa123");
+
+        //Login na API e recebimento de TOKEN
+        String token = given()
+                .body(login) //Passando o login em formato de MAP para melhor organização
+        .when()
+                .post("/signin")
+        .then()
+                .statusCode(200)
+                .extract().path("token")
+        ;
+        //System.out.println(token);
+
+        //Incluir conta
+
+        given()
+                .header("Authorization", "JWT " + token) //API's mais recentes ao inves de 'JWT' se usa 'bearer'.
+                .body("{\"nome\": \"conta qualquer\"}") //Passa o JSON com as informações que se quer
+        .when()
+                .post("/contas")//Método post para envio
+        .then()
+                .statusCode(201)
+        ;
+
+    }
 }
+
+
